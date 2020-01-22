c0b3cc65611299dd63cdaf75daec01abecd4c764
Aaron Hooper <rnjckhpr@gmail.com>
2020-01-22 17:05:01 +0000

    Change URLForm to function component

--- src/URLForm/index.js
+++ src/URLForm/index.js
@@ -9,27 +9,21 @@ import {
 import URLTextBox from './URLTextBox';
 import DownloadButton from './DownloadButton';
 
-class URLForm extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <Container>
-        <Row>
-          <Col md="6" className="mx-auto">
-            <URLTextBox
-              text={this.props.text}
-              onKeyPress={this.props.onKeyPress}
-              onChange={this.props.onChange} />
-            <DownloadButton
-              onClick={this.props.onButtonClick} />
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+function URLForm(props) {
+  return (
+    <Container>
+      <Row>
+        <Col md="6" className="mx-auto">
+          <URLTextBox
+            text={props.text}
+            onKeyPress={props.onKeyPress}
+            onChange={props.onChange} />
+          <DownloadButton
+            onClick={props.onButtonClick} />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default URLForm;
